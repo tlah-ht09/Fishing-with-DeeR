a3af606ccfe04d729d643f7e1e092e2470f8b451
정현태 <jht3021@gmail.com>
2025-11-24 17:55:52 +0900

    페이지 설정

---
+++ src/pages/main/style.js
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+import fishingGround from "../../assets/fishingGround.png";
+
+export const Main = styled.div`
+  width: 1370px;
+  height: 900px;
+  flex-shrink: 0;
+
+  background: url(${fishingGround}) lightgray 50% / cover no-repeat;
+`;
