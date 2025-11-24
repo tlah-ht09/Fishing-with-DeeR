8f01398aaac5c841d7bb3bfa3506f046d3d06ed8
이호준 <hjsj23430@naver.com>
2025-11-24 20:03:03 +0900

    shopscreen

---
+++ src/pages/shop/index.jsx
@@ -0,0 +1,30 @@
+import * as _ from './style'
+
+export const Shop = () => {
+    return(
+        <_.main>
+            <_.fishingRopDiv>
+                <_.fishingRop> 
+                    <_.fishingRopImg></_.fishingRopImg>
+                    <_.fishingRopT></_.fishingRopT>
+                </_.fishingRop>
+                <_.fishingRop2>
+                    <_.fishingRopImg></_.fishingRopImg>
+                    <_.fishingRopT></_.fishingRopT>
+                </_.fishingRop2>
+                <_.fishingRop3>
+                    <_.fishingRopImg></_.fishingRopImg>
+                    <_.fishingRopT></_.fishingRopT>
+                </_.fishingRop3>
+                <_.fishingRop4>
+                    <_.fishingRopImg></_.fishingRopImg>
+                    <_.fishingRopT></_.fishingRopT>
+                </_.fishingRop4>
+                <_.fishingRop5>
+                    <_.fishingRopImg2></_.fishingRopImg2>
+                    <_.fishingRopT></_.fishingRopT>
+                </_.fishingRop5>
+            </_.fishingRopDiv>
+        </_.main>
+    )
+}
---
+++ src/pages/shop/style.js
@@ -0,0 +1,82 @@
+import styled from '@emotion/styled'
+
+export const main = styled.div`
+    display: flex;
+    width: 1370px;
+    height: 900px;
+    justify-content: center;
+    align-items: center;
+    background: #FFF;
+    flex-shrink: 0;
+    background: url(<path-to-image>) lightgray 50% / cover no-repeat;
+`;
+
+export const fishingRopDiv = styled.div`
+    display: inline-flex;
+    align-items: center;
+    gap: 60px;
+`
+
+export const fishingRop = styled.div`
+    display: flex;
+    width: 234px;
+    flex-direction: column;
+    align-items: center;
+    gap: 17px;
+`
+
+export const fishingRop2 = styled.div`
+    display: flex;
+    width: 245px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 17px;
+`
+
+export const fishingRop3 = styled.div`
+    display: flex;
+    width: 337px;
+    flex-direction: column;
+    align-items: center;
+    gap: 17px;
+`
+
+export const fishingRop4 = styled.div`
+    display: flex;
+    width: 235px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 17px;
+`
+
+export const fishingRop5 =styled.div`
+    display: flex;
+    width: 463px;
+    flex-direction: column;
+    align-items: center;
+    gap: 14px;
+`
+
+export const fishingRopImg = styled.img`
+    height: 234px;
+    align-self: stretch;
+    aspect-ratio: 1/1;
+    border-radius: 20px;
+
+`
+export const fishingRopT = styled.p`
+    color: #FFF;
+    text-align: center;
+    font-family: Inter; 
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+`
+
+export const fishingRopImg2 = styled.img`
+    height: 234px;
+    align-self: stretch;    
+    aspect-ratio: 463/234;
+    border-radius: 20px;   
+`
