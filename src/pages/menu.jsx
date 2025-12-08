7622318747cbd8e0d143ef7d93676c2448a13224
정현태 <jht3021@gmail.com>
2025-12-08 17:53:41 +0900

    사용자 이름 받기

--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -1,10 +1,14 @@
 import * as _ from "./style";
 import menu_img from "../assets/menu_img.png";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Menu = () => {
   const navigate = useNavigate();
+
+  const [userName, setUserName] = useState(
+    prompt("당신의 이름은 무엇인가요?", "홍길동")
+  );
   return (
     <_.background src={menu_img}>
       <_.menu_bar>
